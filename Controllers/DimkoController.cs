c82d048230a66b853f00a9259141baff16f1fbcb
dmytro.zhluktenko <dmytro.zhluktenko@eleks.com>
2020-10-09 10:30:18 +0300

    Controller is not able to resolve URLs correctly.

---
+++ Controllers/DimkoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiThingTest.Controllers
+{
+	[ApiController]
+	[Route("api/dimko")]
+	public sealed class DimkoController : ControllerBase
+	{
+		[HttpGet, Route("must-be-nice")]
+		public bool MustBeNice() => true;
+
+		[HttpGet, Route("/must-not-be-nice")]
+		public bool MustNotBeNice() => true;
+	}
+}
